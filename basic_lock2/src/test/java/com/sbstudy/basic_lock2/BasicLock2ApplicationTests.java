7b9297835bfb0a6e2ac2f71d3de90b4c5adcdf21
이경용 <sleekydz86@icloud.com>
2024-03-05 14:23:12 +0900

    스부링부트 비관적인 락 스터디 셋팅

---
+++ basic_lock2/src/test/java/com/sbstudy/basic_lock2/BasicLock2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbstudy.basic_lock2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicLock2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
