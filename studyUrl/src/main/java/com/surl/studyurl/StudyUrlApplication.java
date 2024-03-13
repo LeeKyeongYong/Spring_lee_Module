80bc4b2a3dabbb14e6b1b66f05dd93573fe89679
이경용 <sleekydz86@icloud.com>
2024-03-13 19:14:20 +0900

    라이브러리 및 의존성 셋팅

--- studyUrl/src/main/java/com/surl/studyurl/StudyUrlApplication.java
+++ studyUrl/src/main/java/com/surl/studyurl/StudyUrlApplication.java
@@ -2,8 +2,10 @@ package com.surl.studyurl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StudyUrlApplication {
 
     public static void main(String[] args) {
