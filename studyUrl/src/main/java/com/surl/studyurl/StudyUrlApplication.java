9740d5513a0e1bfedf7c3a6a7c4916375bb65121
이경용 <sleekydz86@icloud.com>
2024-03-13 18:58:39 +0900

    URL 단축 스터디 학습

---
+++ studyUrl/src/main/java/com/surl/studyurl/StudyUrlApplication.java
@@ -0,0 +1,13 @@
+package com.surl.studyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyUrlApplication.class, args);
+    }
+
+}
---
+++ studyUrl/src/test/java/com/surl/studyurl/StudyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surl.studyurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
