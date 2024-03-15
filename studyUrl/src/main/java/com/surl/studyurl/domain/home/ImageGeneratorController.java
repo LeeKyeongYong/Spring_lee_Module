f013653849cc715f0b5003ac6093751cfe4ccdc5
이경용 <sleekydz86@icloud.com>
2024-03-15 20:36:00 +0900

    제공된 Spring Ai 라이브러리를 사용하여 이미지 생성시키는 컨트롤

---
+++ studyUrl/src/main/java/com/surl/studyurl/domain/home/ImageGeneratorController.java
@@ -0,0 +1,34 @@
+package com.surl.studyurl.domain.home;
+
+import org.springframework.ai.image.Image;
+import org.springframework.ai.image.ImageClient;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageGeneratorController {
+
+
+    @Autowired
+    private ImageClient openAiImageClient;
+
+    @GetMapping("/image")
+    public Image getImage(@RequestParam String imagePrompt){
+        ImageResponse response = openAiImageClient.call(
+                new ImagePrompt(imagePrompt,
+                        OpenAiImageOptions.builder()
+                                .withQuality("hd")
+                                .withN(4)
+                                .withHeight(1024)
+                                .withWidth(1024).build())
+
+        );
+        return response.getResult().getOutput();
+    }
+}
