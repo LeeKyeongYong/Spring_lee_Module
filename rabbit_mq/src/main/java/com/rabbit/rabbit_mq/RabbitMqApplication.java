512dc23686d06d8731d3626778e5ca42f46a00fe
이경용 <sleekydz86@icloud.com>
2024-03-20 19:57:22 +0900

    rabbit_mq프로젝트 추가

---
+++ rabbit_mq/src/main/java/com/rabbit/rabbit_mq/RabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit.rabbit_mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqApplication.class, args);
+    }
+
+}
---
+++ rabbit_mq/src/test/java/com/rabbit/rabbit_mq/RabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbit.rabbit_mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
