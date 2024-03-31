6a23230dc3b6e6a787e506bc191fa62c0867b7eb
이경용 <sleekydz86@icloud.com>
2024-03-31 19:10:31 +0900

    검색엔진 기능 스터디 프로젝트 추가

---
+++ sb_search/src/main/java/com/example/sb_search/SbSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbSearchApplication.class, args);
+    }
+
+}
---
+++ sb_search/src/test/java/com/example/sb_search/SbSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
