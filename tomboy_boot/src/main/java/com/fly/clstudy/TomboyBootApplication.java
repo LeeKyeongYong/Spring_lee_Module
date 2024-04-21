1503ab6b3e5cf8fefb0ffa3971be1c70c787dc76
이경용 <sleekydz86@icloud.com>
2024-04-21 17:04:28 +0900

    flying 업데이트 버전

---
+++ tomboy_boot/src/main/java/com/fly/clstudy/TomboyBootApplication.java
@@ -0,0 +1,13 @@
+package com.fly.clstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomboyBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomboyBootApplication.class, args);
+    }
+
+}
---
+++ tomboy_boot/src/test/java/com/fly/clstudy/TomboyBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fly.clstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomboyBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
