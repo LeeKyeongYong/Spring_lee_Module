8a613cda70237adecbbb28aba83cb6b3b8b2edf5
이경용 <sleekydz86@icloud.com>
2024-04-21 17:16:12 +0900

    sur의 body추가

--- tomboy_boot/src/main/java/com/fly/clstudy/sur/controller/SurlController.java
+++ tomboy_boot/src/main/java/com/fly/clstudy/sur/controller/SurlController.java
@@ -1,11 +1,13 @@
 package com.fly.clstudy.sur.controller;
 
 import com.fly.clstudy.sur.entity.Surl;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -16,6 +18,35 @@ public class SurlController {
     @GetMapping("/add")
     @ResponseBody
     public Surl add(String body, String url) {
+        Surl surl = Surl
+                .builder()
+                .id(++surlsLastId)
+                .body(body)
+                .url(url)
+                .build();
+
+        surls.add(surl);
+
+        return surl;
+    }
+    @GetMapping("/s/{body}/**")
+    @ResponseBody
+    public Surl add(
+            @PathVariable String body,
+            HttpServletRequest req
+    ) {
+        String url = req.getRequestURI();
+
+        if ( req.getQueryString() != null ) {
+            url += "?" + req.getQueryString();
+        }
+
+        String[] urlBits = url.split("/", 4);
+
+        System.out.println("Arrays.toString(urlBits) : " + Arrays.toString(urlBits));
+
+        url = urlBits[3];
+
         Surl surl = Surl
                 .builder()
                 .id(++surlsLastId)
