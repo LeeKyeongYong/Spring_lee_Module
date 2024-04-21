04f3d7820ae5c73269d146075411f7964a082d48
이경용 <sleekydz86@icloud.com>
2024-04-21 17:42:37 +0900

    save하고 해당내용 조회

--- tomboy_boot/src/main/java/com/fly/clstudy/article/entity/Article.java
+++ tomboy_boot/src/main/java/com/fly/clstudy/article/entity/Article.java
@@ -3,10 +3,7 @@ package com.fly.clstudy.article.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
--- tomboy_boot/src/main/java/com/fly/clstudy/global/initdata/NotProd.java
+++ tomboy_boot/src/main/java/com/fly/clstudy/global/initdata/NotProd.java
@@ -3,27 +3,37 @@ package com.fly.clstudy.global.initdata;
 import com.fly.clstudy.article.entity.Article;
 import com.fly.clstudy.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 // !prod == dev or test
 @Profile("!prod")
 @Configuration
 @RequiredArgsConstructor
 public class NotProd {
+    @Lazy
+    @Autowired
+    private NotProd self;
     private final ArticleRepository articleRepository;
 
     @Bean
     public ApplicationRunner initNotProd() {
-        return new ApplicationRunner() {
-            @Override
-            @Transactional
-            public void run(ApplicationArguments args) throws Exception {
-                if (articleRepository.count() > 0) return;
+        return args -> {
+            self.work1();
+            self.work2();
+        };
+    }
+        @Transactional
+        public void work1() {
+            if (articleRepository.count() > 0) return;
 
             Article article1 = Article.builder()
                     .title("제목 1")
@@ -37,8 +47,14 @@ public class NotProd {
 
             articleRepository.save(article1);
             articleRepository.save(article2);
+            article2.setTitle("제목!!");
 
             articleRepository.delete(article1);
-        };
-    }
+        }
+
+        @Transactional
+        public void work2() {
+            Article article = articleRepository.findById(2L).get();
+            List<Article> articles = articleRepository.findAll();
+        }
 }
