3b48b25ba791786591352b19dcedc58dd90e7f24
이경용 <sleekydz86@icloud.com>
2024-04-24 15:35:08 +0900

    LocalDateTime Article클래스에 추가

--- tomboy_boot/src/main/java/com/fly/clstudy/TomboyBootApplication.java
+++ tomboy_boot/src/main/java/com/fly/clstudy/TomboyBootApplication.java
@@ -2,8 +2,10 @@ package com.fly.clstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TomboyBootApplication {
 
     public static void main(String[] args) {
--- tomboy_boot/src/main/java/com/fly/clstudy/article/entity/Article.java
+++ tomboy_boot/src/main/java/com/fly/clstudy/article/entity/Article.java
@@ -1,9 +1,15 @@
 package com.fly.clstudy.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -13,10 +19,15 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
     private String title;
     private String body;
 }
