a4f8d622438c421ec95f4e137a88a284cf8b9d37
이경용 <sleekydz86@icloud.com>
2024-04-26 15:39:08 +0900

    Valid 어노테이션 추가

--- tomboy_boot/src/main/java/com/fly/clstudy/data/MemberJoinReqBody.java
+++ tomboy_boot/src/main/java/com/fly/clstudy/data/MemberJoinReqBody.java
@@ -2,11 +2,16 @@ package com.fly.clstudy.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 
 @AllArgsConstructor
 @Getter
 public class MemberJoinReqBody {
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String username;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+    @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
 }
--- tomboy_boot/src/main/java/com/fly/clstudy/member/controller/ApiMemberController.java
+++ tomboy_boot/src/main/java/com/fly/clstudy/member/controller/ApiMemberController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -23,16 +25,8 @@ public class ApiMemberController {
     private final MemberService memberService;
 
     @PostMapping("")
-    public RespData<Member> join(@RequestBody MemberJoinReqBody reqBody){
-        if(UtStr.str.isBlank(reqBody.getUsername())){
-            throw new GlobalException("400-1","아이디어를 입력해주세요.");
-        }
-        if(UtStr.str.isBlank(reqBody.getUsername())){
-            throw new GlobalException("400-2","비밀번호를 입력해주세요.");
-        }
-        if(UtStr.str.isBlank(reqBody.getUsername())){
-            throw new GlobalException("400-3","닉네임을를 입력해주세요.");
-        }
+    public RespData<Member> join(@RequestBody @Valid MemberJoinReqBody reqBody){
+
         return memberService.join(reqBody.getUsername(),reqBody.getPassword(),reqBody.getNickname());
     }
 
