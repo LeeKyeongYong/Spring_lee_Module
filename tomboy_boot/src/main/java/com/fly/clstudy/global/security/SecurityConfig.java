697bcb4ea270433f57a49b5a66c380554fe25a95
이경용 <sleekydz86@icloud.com>
2024-05-10 20:52:00 +0900

    security config 생성

---
+++ tomboy_boot/src/main/java/com/fly/clstudy/global/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.fly.clstudy.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest()
+                                .authenticated()
+                )
+                .headers(
+                        headers ->
+                                headers.frameOptions(
+                                        frameOptions ->
+                                                frameOptions.sameOrigin()
+                                )
+                )
+                .csrf(
+                        csrf ->
+                                csrf.disable()
+                )
+                .formLogin(
+                        formLogin ->
+                                formLogin
+                                        .permitAll()
+                )
+                .logout(
+                        logout ->
+                                logout
+                                        .logoutRequestMatcher(
+                                                new AntPathRequestMatcher("/logout")
+                                        )
+                );
+
+        return http.build();
+    }
+}
