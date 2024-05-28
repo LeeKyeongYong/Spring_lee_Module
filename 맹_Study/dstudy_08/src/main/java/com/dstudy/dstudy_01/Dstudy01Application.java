c0f83cc66868f6c9a631e9b36277f1582d77a944
이경용 <sleekydz86@naver.com>
2024-05-29 01:12:46 +0900

    스터디 2차시부터 16차시 셋팅

---
+++ 맹_Study/dstudy_08/src/main/java/com/dstudy/dstudy_01/Dstudy01Application.java
@@ -0,0 +1,13 @@
+package com.dstudy.dstudy_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dstudy01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dstudy01Application.class, args);
+    }
+
+}
---
+++ 맹_Study/dstudy_14/src/test/java/com/dstudy/dstudy_01/Dstudy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dstudy.dstudy_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dstudy01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
