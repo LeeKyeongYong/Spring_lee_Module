0afa5c2ef5d4be2e02a938d8228a7832617fa522
이경용 <sleekydz86@icloud.com>
2024-06-21 18:50:15 +0900

    플로젝트생성

---
+++ bt_msa/src/test/java/com/mstudy/bt_msa/BtMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstudy.bt_msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ 맹_Study/testSm/src/main/java/com/daeun/testsm/TestController.java
@@ -0,0 +1,2 @@
+package com.daeun.testsm;public class TestController {
+}
