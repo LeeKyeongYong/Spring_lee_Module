1356766100b96f69e4d4fa10e60c4de227c09ad5
이경용 <sleekydz86@naver.com>
2024-06-21 19:12:04 +0900

    work.

--- 맹_Study/testSm/src/main/java/com/daeun/testsm/TestController.java
+++ 맹_Study/testSm/src/main/java/com/daeun/testsm/TestController.java
@@ -1,2 +1,13 @@
-package com.daeun.testsm;public class TestController {
+package com.daeun.testsm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    public String Test(){
+        return "test";
+    }
 }
---
+++ 맹_Study/testSm/src/test/java/com/daeun/testsm/TestSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daeun.testsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
