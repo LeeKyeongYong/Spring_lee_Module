f1c0e865ae70359012c15a9d6dfec5a4cc5c3a06
이경용 <sleekydz86@icloud.com>
2024-06-21 20:36:03 +0900

    각 모듈에서 공통지식(로직과 설정) 가져다가 쓰게 설정

--- bt_msa/module_app_member/src/main/java/com/example/module_app_member/domain/member/controller/MemberController.java
+++ bt_msa/module_app_member/src/main/java/com/example/module_app_member/domain/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.module_app_member.domain.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+
+    @Value("${custom.site.name}")
+    private String siteName;
+
     @GetMapping("")
     @ResponseBody
     public String index() {
         int a = 10 + 20;
         return "환영합니다.";
+
+        StrUtil.isEmpty("test");
+
+        return siteName + "에 오신것을 환영합니다.";
     }
 }
---
+++ bt_msa/module_common_util/src/main/java/com/example/module_common_util/util/StrUtil.java
@@ -0,0 +1,7 @@
+package com.example.module_common_util.util;
+
+public class StrUtil {
+    public static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+}
