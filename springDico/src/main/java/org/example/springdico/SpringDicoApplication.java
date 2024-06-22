3a650a76398030a8a497f5f457161c8178bb96ad
이경용 <sleekydz86@icloud.com>
2024-06-22 23:25:55 +0900

    디코연동 셋팅

---
+++ springDico/src/main/java/org/example/springdico/SpringDicoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springdico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDicoApplication.class, args);
+    }
+
+}
---
+++ springDico/src/test/java/org/example/springdico/SpringDicoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
