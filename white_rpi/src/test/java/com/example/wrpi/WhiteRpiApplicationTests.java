6e05bf14881da9c0e18a628226cad59f3cf59e57
이경용 <sleekydz86@icloud.com>
2024-06-25 00:32:43 +0900

    white Rest버전 스터디 토론회

---
+++ white_rpi/src/test/java/com/example/wrpi/WhiteRpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wrpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteRpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
