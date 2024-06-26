ce38c6277013806f3ae18d5c3d921f789cb917af
이경용 <sleekydz86@icloud.com>
2024-06-26 23:11:24 +0900

    슬랙버전 업데이트

---
+++ slackSprig/src/main/java/com/alarmstudy/slacksprig/SlackSprigApplication.java
@@ -0,0 +1,13 @@
+package com.alarmstudy.slacksprig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackSprigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackSprigApplication.class, args);
+    }
+
+}
