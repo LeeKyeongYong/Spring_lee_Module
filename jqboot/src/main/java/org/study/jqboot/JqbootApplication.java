cb44689e385b875c81b7133d999104020d04a68a
이경용 <sleekydz86@icloud.com>
2024-06-29 00:56:24 +0900

    jooq 베이직 환경셋팅

---
+++ jqboot/src/main/java/org/study/jqboot/JqbootApplication.java
@@ -0,0 +1,13 @@
+package org.study.jqboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JqbootApplication.class, args);
+    }
+
+}
---
+++ jqboot/src/test/java/org/study/jqboot/JqbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.jqboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JqbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
