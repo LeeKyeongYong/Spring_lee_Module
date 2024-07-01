1d3ce482c7a7400c7d309b22b8c2a47128fbe428
이경용 <sleekydz86@icloud.com>
2024-07-01 21:04:27 +0900

    토스페이먼츠 아티클 기반 결제 시스템 멱등성 적용하기 학습

---
+++ payment_study/src/main/java/com/ps/idempot/PaymentStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ps.idempot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentStudyApplication.class, args);
+    }
+
+}
---
+++ payment_study/src/test/java/com/ps/idempot/PaymentStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.idempot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
