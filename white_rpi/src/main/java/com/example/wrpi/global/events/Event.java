02f2dee7fa5467913b89c6c2b505fc60d65c46e5
이경용 <sleekydz86@icloud.com>
2024-07-20 13:22:43 +0900

    feat:boot3.3.2 버전에 맞춰서 의존성 변경-Add create events test

--- white_rpi/src/main/java/com/example/wrpi/global/events/Event.java
+++ white_rpi/src/main/java/com/example/wrpi/global/events/Event.java
@@ -17,15 +17,15 @@ public class Event {
     private boolean free;
     private EventStatus eventStatus = EventStatus.DRAFT;
 
-    private String name;
-    private String description;
-    private LocalDateTime beginEnrollmentDateTime;
-    private LocalDateTime closeEnrollmentDateTime;
-    private LocalDateTime beginEventDateTime;
-    private LocalDateTime endEventDateTime;
+    private String name; //이벤트 이름
+    private String description; //이벤트 설명
+    private LocalDateTime beginEnrollmentDateTime; //이벤트 등록시작시간
+    private LocalDateTime closeEnrollmentDateTime; //이벤트 종료시간
+    private LocalDateTime beginEventDateTime; //이벤트 시작일시
+    private LocalDateTime endEventDateTime; //이벤트 종료일시
     private String location; // (optional) 이게 없으면 온라인 모임
-    private int basePrice; // (optional)
-    private int maxPrice; // (optional)
-    private int limitOfEnrollment;
+    private int basePrice; // (optional) 참가비 기본
+    private int maxPrice; // (optional) 참가비 최대 (*) maxprice가 0이고 basePrice가 값이 존재하면 무한 경매가 됨
+    private int limitOfEnrollment; //등록제한
 
 }
---
+++ white_rpi/src/test/java/com/example/wrpi/global/events/EventControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.wrpi.global.events;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void createEvent()throws Exception{
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON))
+                        .andExpect(status().isCreated());
+    }
+
+}
