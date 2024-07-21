25b8ab5c8ea232c2dfafb1fdaa837d4e1d6d129b
이경용 <sleekydz86@icloud.com>
2024-07-21 21:08:11 +0900

    feat:Parameterized tests

--- white_rpi/src/test/java/com/example/wrpi/global/events/EventTest.java
+++ white_rpi/src/test/java/com/example/wrpi/global/events/EventTest.java
@@ -1,9 +1,14 @@
 package com.example.wrpi.global.events;
 
 import com.example.wrpi.domain.Events.entity.Event;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
     @Test
@@ -32,4 +37,55 @@ public class EventTest {
         assertThat(event.getDescription()).isEqualTo(description);
 
     }
+
+    @Test
+    @Parameters
+    public void testFree(int basePrice,int maxPrice,boolean isFree){
+
+        //Given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        //When
+        event.update();
+
+        //Then
+        assertThat(event.isFree()).isFalse();
+
+    }
+
+    private Object[] parametersForTestFree(){
+      return new Object[]{
+        new Object[]{0,0,true},
+              new Object[]{100,0,false},
+              new Object[]{0,100,false},
+              new Object[]{100,200,false}
+
+      };
+    }
+
+    @Test
+    @Parameters
+    public void testofline(String location,boolean isOffline){
+        //Given
+        Event event = Event.builder()
+                .location(location)
+                .build();
+        //when
+            event.update();
+
+        //then
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
+
+    private Object[] parametersForTestOffline(){
+        return new Object[]{
+                new Object[]{"강남",true},
+                new Object[]{null,false},
+                new Object[]{"       ",false}
+        };
+    }
+
 }
