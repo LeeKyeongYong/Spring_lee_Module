a345a059149d6ed562491f51a62e4629e4d2dfcd
이경용 <sleekydz86@icloud.com>
2024-07-22 15:26:10 +0900

    feat: Test DB and config

--- white_rpi/src/test/java/com/example/wrpi/global/events/EventControllerTests.java
+++ white_rpi/src/test/java/com/example/wrpi/global/events/EventControllerTests.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTests {
 
     @Autowired
