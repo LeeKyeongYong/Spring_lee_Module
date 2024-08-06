43f944ac2ff671b8a5248d451874e38d9bee8695
이경용 <sleekydz86@naver.com>
2024-08-06 23:34:58 +0900

    rest-api 정리

--- k_rstudy/src/main/kotlin/com/krstudy/kapi/domain/home/Controller/HomeController.kt
+++ k_rstudy/src/main/kotlin/com/krstudy/kapi/domain/home/Controller/HomeController.kt
@@ -1,9 +1,15 @@
 package com.krstudy.kapi.com.krstudy.kapi.domain.home.Controller
 
+import com.krstudy.kapi.com.krstudy.kapi.domain.post.service.PostService
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+<<<<<<< Updated upstream
 import com.krstudy.kapi.com.krstudy.kapi.global.https.ReqData
 import com.krstudy.kapi.domain.post.service.PostService
+=======
+import com.krstudy.kapi.com.krstudy.kapi.global.https.Req
+import com.krstudy.kapi.com.krstudy.kapi.global.https.ReqData
+>>>>>>> Stashed changes
 
 @Controller
 class HomeController(
