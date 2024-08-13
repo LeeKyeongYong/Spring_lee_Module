11b4e9615f360f9e03e41124fa0d0743ae7a95cf
이경용 <sleekydz86@naver.com>
2024-08-13 15:09:49 +0900

    mstudy 프로젝트 셋팅

---
+++ lky_msa/mstudy/src/main/java/com/lky/mstudy/MstudyApplication.java
@@ -0,0 +1,13 @@
+package com.lky.mstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MstudyApplication.class, args);
+    }
+
+}
---
+++ lky_msa/mstudy/src/test/java/com/lky/mstudy/MstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lky.mstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
