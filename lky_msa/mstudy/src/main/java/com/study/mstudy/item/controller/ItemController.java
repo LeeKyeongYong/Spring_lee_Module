f9f1ccb73a13af8c14798220a81b94ae8b8cc222
이경용 <sleekydz86@naver.com>
2024-08-13 18:31:39 +0900

    Enhance validation for ItemDTO and ItemController
    - Added @NotBlank, @Size, and @Positive annotations to ItemDTO fields to enforce data constraints.
    - Updated ItemController's add method with @Valid annotation to trigger validation of incoming ItemDTO.
    - Ensured proper error handling for validation failures in the controller.

--- lky_msa/mstudy/src/main/java/com/study/mstudy/item/controller/ItemController.java
+++ lky_msa/mstudy/src/main/java/com/study/mstudy/item/controller/ItemController.java
@@ -1,14 +1,18 @@
 package com.study.mstudy.item.controller;
 
+
 import com.study.mstudy.item.dto.ItemDTO;
 import com.study.mstudy.item.dto.ResponseDTO;
 import com.study.mstudy.item.service.ItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="v1/item")
@@ -16,19 +20,16 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ItemController {
 
-    @Autowired
-    private ItemService itemService;
+    private final ItemService itemService;
 
-    @RequestMapping(value="/add",method= RequestMethod.POST)
+    @RequestMapping(value="/add", method=RequestMethod.POST)
     public ResponseEntity<ResponseDTO> add(@Valid @RequestBody ItemDTO itemDTO){
-
-        ResponseDTO.ResponseDTOBuilder responseDTOBuilder = ResponseDTO.builder();
+        ResponseDTO.ResponseDTOBuilder responseBuilder = ResponseDTO.builder();
 
         itemService.insertItem(itemDTO);
-        log.debug("request add item id= {}",itemDTO.getId());
+        log.debug("request add item id = {}", itemDTO.getId());
 
-        responseDTOBuilder.code("200").message("success");
-        return ResponseEntity.ok(responseDTOBuilder.build());
+        responseBuilder.code("200").message("success");
+        return ResponseEntity.ok(responseBuilder.build());
     }
-
 }
--- lky_msa/mstudy/src/main/java/com/study/mstudy/item/dto/ItemDTO.java
+++ lky_msa/mstudy/src/main/java/com/study/mstudy/item/dto/ItemDTO.java
@@ -1,15 +1,25 @@
 package com.study.mstudy.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ItemDTO {
     @NotBlank(message = "ID는 필수 입력 값입니다.")
+    @Size(max=10, message = "ID는 크기 10이하까지 작성가능합니다.")
     private String id;
+
+    @Size(max=20, message = "이름은 20자까지 작성가능합니다.")
     private String name;
+
+    @Size(max=200, message = "설명은 최대 200자까지 작성가능합니다.")
     private String description;
+
+    @Positive
     private long count;
+
     private String regDts;
     private String updDts;
 }
