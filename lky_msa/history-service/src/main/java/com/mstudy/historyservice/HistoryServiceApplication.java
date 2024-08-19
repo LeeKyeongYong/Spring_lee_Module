59e4f80325c0224037f1ced4f60fa7a25b2b21d5
이경용 <sleekydz86@naver.com>
2024-08-19 13:09:42 +0900

    history프로젝트생성

---
+++ lky_msa/history-service/src/main/java/com/mstudy/historyservice/HistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mstudy.historyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoryServiceApplication.class, args);
+    }
+
+}
---
+++ lky_msa/history-service/src/test/java/com/mstudy/historyservice/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstudy.historyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
