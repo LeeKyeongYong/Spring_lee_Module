a9de475d7b3bb6fee7ce0f9e6e39f48f2a50983e
이경용 <sleekydz86@naver.com>
2024-08-27 16:35:07 +0900

    뷰 생성

--- k_rstudy/src/main/resources/db/migration/k_db.sql
+++ k_rstudy/src/main/resources/db/migration/k_db.sql
@@ -1,4 +1,5 @@
 CREATE FUNCTION get_author_name(author_id BIGINT) RETURNS VARCHAR(255)
+    DETERMINISTIC
 BEGIN
     DECLARE author_name VARCHAR(255);
 SELECT username INTO author_name FROM member WHERE id = author_id;
@@ -10,15 +11,18 @@ SELECT
     m.id AS member_id,
     m.userid,
     m.username,
-    m.roleType,
+    m.role_type,
     m.password,
     CASE
-        WHEN m.roleType = 'ADMIN' THEN 'ROLE_ADMIN'
-        WHEN m.roleType = 'USER' THEN 'ROLE_USER'
-        ELSE 'ROLE_UNKNOWN'
+        WHEN m.role_type = 'ROLE_ADMIN' THEN '관리자'
+        WHEN m.role_type = 'ROLE_MEMBER' THEN '일반 사용자'
+        WHEN m.role_type = 'ROLE_HEADHUNTER' THEN '헤드 헌터'
+        WHEN m.role_type = 'ROLE_MANAGER' THEN '기업 관리자'
+        WHEN m.role_type = 'ROLE_HR' THEN '채용 담당자'
+        ELSE 'ROLE_MEMBER'
         END AS authorities,
     CASE
-        WHEN m.roleType = 'ADMIN' THEN TRUE
+        WHEN m.role_type = 'ROLE_ADMIN' THEN TRUE
         ELSE FALSE
         END AS isAdmin
 FROM
