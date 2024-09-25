345a6407388b15a2117d54e7b331e8ec96b27209
이경용 <sleekydz86@naver.com>
2024-09-25 18:15:48 +0900

    feat:init - 하이버네이트 복습을 위한 커밋

---
+++ Hibernate2/src/main/java/com/jstudy/hibernate2/Hibernate2Application.java
@@ -0,0 +1,13 @@
+package com.jstudy.hibernate2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hibernate2Application.class, args);
+    }
+
+}
---
+++ Hibernate2/src/test/java/com/jstudy/hibernate2/Hibernate2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstudy.hibernate2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hibernate2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
