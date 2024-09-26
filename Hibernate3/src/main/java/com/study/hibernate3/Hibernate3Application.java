6ef97d8472a37772a60a3ee86535b6afcdc0c66c
이경용 <sleekydz86@naver.com>
2024-09-26 17:11:38 +0900

    하이버네이트 레거시 리마인드 init

---
+++ Hibernate3/src/main/java/com/study/hibernate3/Hibernate3Application.java
@@ -0,0 +1,13 @@
+package com.study.hibernate3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hibernate3Application.class, args);
+    }
+
+}
