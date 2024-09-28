e495985f3a58b35c5831b437d559ef7973fcbd0e
이경용 <sleekydz86@naver.com>
2024-09-28 16:49:15 +0900

    new Ios Init

---
+++ AppleLoginStudy/src/main/java/com/appleloginstudy/AppleLoginStudyApplication.java
@@ -0,0 +1,13 @@
+package com.appleloginstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleLoginStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleLoginStudyApplication.class, args);
+    }
+
+}
---
+++ AppleLoginStudy/src/test/java/com/appleloginstudy/AppleLoginStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appleloginstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleLoginStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
