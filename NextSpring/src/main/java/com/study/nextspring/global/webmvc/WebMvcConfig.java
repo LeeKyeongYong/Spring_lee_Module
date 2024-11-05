6fe828722530d5f56f1c54d9d3e0a22bda166c7c
이경용 <sleekydz86@naver.com>
2024-11-05 21:36:06 +0900

    feat: Allow CORS for /api/** requests

---
+++ NextSpring/src/main/java/com/study/nextspring/global/webmvc/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.study.nextspring.global.webmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:3000"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
