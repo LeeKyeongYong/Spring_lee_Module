1d2ab634da8dffb9ef1869c94f3aade3908c36c6
이경용 <sleekydz86@naver.com>
2024-11-05 19:38:37 +0900

    feat:스프링 시큐리티 설정, 일단 모든 요청을 허용
    - 백엔드  시큐리티 생성

---
+++ NextSpring/src/main/java/com/study/nextspring/global/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.study.nextspring.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
