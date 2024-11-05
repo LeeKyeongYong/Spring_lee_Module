c2b57846386b0f17a12bb1922a587e5a6c4c8e9c
이경용 <sleekydz86@naver.com>
2024-11-05 19:14:10 +0900

    feat:nextSpring Part one init
    - next와 스프링 연동 part 레벨 1.

---
+++ NextSpring/src/test/java/com/study/nextspring/NextSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.nextspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
