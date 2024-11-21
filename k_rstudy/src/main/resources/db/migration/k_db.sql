94c7e66a06827b6893721f93d3323ff3a3df4af3
이경용 <sleekydz86@naver.com>
2024-11-22 03:08:53 +0900

    feat:정리
    카프카용 batch정리

--- k_rstudy/src/main/resources/db/migration/k_db.sql
+++ k_rstudy/src/main/resources/db/migration/k_db.sql
@@ -1,131 +1,36 @@
+CREATE TABLE BATCH_JOB_INSTANCE  (
+                                     JOB_INSTANCE_ID BIGINT  NOT NULL PRIMARY KEY,
+                                     VERSION BIGINT,
+                                     JOB_NAME VARCHAR(100) NOT NULL,
+                                     JOB_KEY VARCHAR(32) NOT NULL,
+                                     constraint JOB_INST_UN unique (JOB_NAME, JOB_KEY)
+) ENGINE=InnoDB;
 
-DROP FUNCTION get_author_name;
+CREATE TABLE BATCH_JOB_EXECUTION  (
+                                      JOB_EXECUTION_ID BIGINT  NOT NULL PRIMARY KEY,
+                                      VERSION BIGINT,
+                                      JOB_INSTANCE_ID BIGINT NOT NULL,
+                                      CREATE_TIME DATETIME(6) NOT NULL,
+                                      START_TIME DATETIME(6) DEFAULT NULL,
+                                      END_TIME DATETIME(6) DEFAULT NULL,
+                                      STATUS VARCHAR(10),
+                                      EXIT_CODE VARCHAR(2500),
+                                      EXIT_MESSAGE VARCHAR(2500),
+                                      LAST_UPDATED DATETIME(6),
+                                      constraint JOB_INST_EXEC_FK foreign key (JOB_INSTANCE_ID)
+                                          references BATCH_JOB_INSTANCE(JOB_INSTANCE_ID)
+) ENGINE=InnoDB;
 
-commit;
+CREATE TABLE BATCH_JOB_EXECUTION_PARAMS  (
+                                             JOB_EXECUTION_ID BIGINT NOT NULL,
+                                             PARAMETER_NAME VARCHAR(100) NOT NULL,
+                                             PARAMETER_TYPE VARCHAR(100) NOT NULL,
+                                             PARAMETER_VALUE VARCHAR(2500),
+                                             IDENTIFYING CHAR(1) NOT NULL,
+                                             constraint JOB_EXEC_PARAMS_FK foreign key (JOB_EXECUTION_ID)
+                                                 references BATCH_JOB_EXECUTION(JOB_EXECUTION_ID)
+) ENGINE=InnoDB;
 
-CREATE FUNCTION get_author_name(author_id BIGINT) RETURNS VARCHAR(255)
-    DETERMINISTIC
-BEGIN
-    DECLARE author_name VARCHAR(255);
-SELECT username INTO author_name FROM member WHERE id = author_id;
-RETURN IFNULL(author_name, '사용자 없음');
-END;
-
-commit;
-
-DROP VIEW IF EXISTS member_view;
-
-commit;
-
-CREATE VIEW member_view AS
-SELECT
-    m.id AS member_id,
-    m.userid,
-    m.username,
-    m.role_type,
-    m.password,
-    CASE
-        WHEN m.role_type = 'ROLE_ADMIN' THEN '관리자'
-        WHEN m.role_type = 'ROLE_MEMBER' THEN '일반 사용자'
-        WHEN m.role_type = 'ROLE_HEADHUNTER' THEN '헤드 헌터'
-        WHEN m.role_type = 'ROLE_MANAGER' THEN '기업 관리자'
-        WHEN m.role_type = 'ROLE_HR' THEN '채용 담당자'
-        ELSE 'ROLE_MEMBER'
-        END AS authorities,
-    CASE
-        WHEN m.role_type = 'ROLE_ADMIN' THEN TRUE
-        ELSE FALSE
-        END AS is_admin
-FROM
-    member m;
-
-DROP VIEW IF EXISTS post_details;
-
-CREATE VIEW post_details AS
-SELECT
-    p.id AS post_id, #게시글 번호
-    p.title AS post_title, #게시글 제목
-    p.create_date AS post_create_date, #게시글 작성일
-    p.modify_date AS post_modify_date, #게시글 수정일
-    p.hit AS post_hit, #게시글 조회수
-    get_author_name(p.author_id) AS post_author_id, #게시글 작성자
-    pc.id AS comment_id, #댓글번호
-    get_author_name(pc.author_id) AS comment_author_id, #댓글작성자
-    pc.create_date AS comment_create_date, #댓글 작성일
-    get_author_name(pl.member_id) AS like_member_id,  #좋아요 작성자
-    pl.create_date AS like_create_date #좋아요 작성일
-FROM
-    post p
-        LEFT JOIN
-    post_comment pc ON p.id = pc.post_id
-        LEFT JOIN
-    post_like pl ON p.id = pl.post_id
-ORDER BY
-    p.id, pc.id, pl.create_date;
-
-
-DROP VIEW IF EXISTS post_like_detail;
-
-CREATE VIEW post_like_detail AS
-sELECT
-    p.id AS post_id,
-    p.title AS post_title,
-    p.create_date AS post_create_date,
-    p.modify_date AS post_modify_date,
-    p.hit AS post_hit,
-    p.author_id AS post_author_id,
-    pl.member_id AS like_member_id,
-    pl.create_date AS like_create_date
-FROM
-    post p
-        LEFT JOIN
-    post_like pl ON p.id = pl.post_id
-ORDER BY
-    p.id, pl.create_date;
-
-DROP VIEW IF EXISTS post_comment_detail;
-
-CREATE VIEW post_comment_detail AS
-SELECT
-    p.id AS post_id,
-    p.title AS post_title,
-    p.create_date AS post_create_date,
-    p.modify_date AS post_modify_date,
-    p.hit AS post_hit,
-    p.author_id AS post_author_id,
-    pc.id AS comment_id,
-    pc.author_id AS comment_author_id,
-    pc.create_date AS comment_create_date
-FROM
-    post p
-        LEFT JOIN
-    post_comment pc ON p.id = pc.post_id
-ORDER BY
-    p.id, pc.id;
-
-DROP VIEW IF EXISTS post_comment_count;
-
-CREATE VIEW post_comment_count AS
-SELECT
-    p.id AS post_id,
-    p.title AS post_title,
-    COUNT(pc.id) AS comment_count
-FROM
-    post p
-        LEFT JOIN
-    post_comment pc ON p.id = pc.post_id
-GROUP BY
-    p.id, p.title;
-
-DROP VIEW IF EXISTS post_like_count;
-
-SELECT
-    p.id AS post_id,
-    p.title AS post_title,
-    COUNT(pl.id) AS like_count
-FROM
-    post p
-        LEFT JOIN
-    post_like pl ON p.id = pl.post_id
-GROUP BY
-    p.id, p.title;
+CREATE SEQUENCE IF NOT EXISTS BATCH_STEP_EXECUTION_SEQ;
+CREATE SEQUENCE IF NOT EXISTS BATCH_JOB_EXECUTION_SEQ;
+CREATE SEQUENCE IF NOT EXISTS BATCH_JOB_SEQ;
