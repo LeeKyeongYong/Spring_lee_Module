4eee2f2d7d220105785ef88c55a76058e0134a25
이경용 <sleekydz86@naver.com>
2024-11-24 00:01:25 +0900

    db정리..

--- k_rstudy/src/main/resources/db/migration/k_db.sql
+++ k_rstudy/src/main/resources/db/migration/k_db.sql
@@ -1,25 +1,12 @@
-CREATE TABLE BATCH_JOB_INSTANCE  (
-                                     JOB_INSTANCE_ID BIGINT  NOT NULL PRIMARY KEY,
-                                     VERSION BIGINT,
-                                     JOB_NAME VARCHAR(100) NOT NULL,
-                                     JOB_KEY VARCHAR(32) NOT NULL,
-                                     constraint JOB_INST_UN unique (JOB_NAME, JOB_KEY)
-) ENGINE=InnoDB;
-
-CREATE TABLE BATCH_JOB_EXECUTION  (
-                                      JOB_EXECUTION_ID BIGINT  NOT NULL PRIMARY KEY,
-                                      VERSION BIGINT,
-                                      JOB_INSTANCE_ID BIGINT NOT NULL,
-                                      CREATE_TIME DATETIME(6) NOT NULL,
-                                      START_TIME DATETIME(6) DEFAULT NULL,
-                                      END_TIME DATETIME(6) DEFAULT NULL,
-                                      STATUS VARCHAR(10),
-                                      EXIT_CODE VARCHAR(2500),
-                                      EXIT_MESSAGE VARCHAR(2500),
-                                      LAST_UPDATED DATETIME(6),
-                                      constraint JOB_INST_EXEC_FK foreign key (JOB_INSTANCE_ID)
-                                          references BATCH_JOB_INSTANCE(JOB_INSTANCE_ID)
-) ENGINE=InnoDB;
+-- DROP TABLE IF EXISTS BATCH_STEP_EXECUTION_CONTEXT;
+-- DROP TABLE IF EXISTS BATCH_JOB_EXECUTION_CONTEXT;
+-- DROP TABLE IF EXISTS BATCH_STEP_EXECUTION;
+-- DROP TABLE IF EXISTS BATCH_JOB_EXECUTION_PARAMS;
+-- DROP TABLE IF EXISTS BATCH_JOB_EXECUTION;
+-- DROP TABLE IF EXISTS BATCH_JOB_INSTANCE;
+-- DROP TABLE IF EXISTS BATCH_STEP_EXECUTION_SEQ;
+-- DROP TABLE IF EXISTS BATCH_JOB_EXECUTION_SEQ;
+-- DROP TABLE IF EXISTS BATCH_JOB_SEQ;
 
 CREATE TABLE BATCH_JOB_EXECUTION_PARAMS  (
                                              JOB_EXECUTION_ID BIGINT NOT NULL,
@@ -31,6 +18,90 @@ CREATE TABLE BATCH_JOB_EXECUTION_PARAMS  (
                                                  references BATCH_JOB_EXECUTION(JOB_EXECUTION_ID)
 ) ENGINE=InnoDB;
 
-CREATE SEQUENCE IF NOT EXISTS BATCH_STEP_EXECUTION_SEQ;
-CREATE SEQUENCE IF NOT EXISTS BATCH_JOB_EXECUTION_SEQ;
-CREATE SEQUENCE IF NOT EXISTS BATCH_JOB_SEQ;
+-- Spring Batch 메타데이터 테이블 생성
+CREATE TABLE BATCH_JOB_INSTANCE (
+                                    JOB_INSTANCE_ID BIGINT NOT NULL PRIMARY KEY,
+                                    VERSION BIGINT,
+                                    JOB_NAME VARCHAR(100) NOT NULL,
+                                    JOB_KEY VARCHAR(32) NOT NULL,
+                                    CONSTRAINT JOB_INST_UN UNIQUE (JOB_NAME, JOB_KEY)
+);
+
+CREATE TABLE BATCH_JOB_EXECUTION (
+                                     JOB_EXECUTION_ID BIGINT NOT NULL PRIMARY KEY,
+                                     VERSION BIGINT,
+                                     JOB_INSTANCE_ID BIGINT NOT NULL,
+                                     CREATE_TIME DATETIME NOT NULL,
+                                     START_TIME DATETIME DEFAULT NULL,
+                                     END_TIME DATETIME DEFAULT NULL,
+                                     STATUS VARCHAR(10),
+                                     EXIT_CODE VARCHAR(2500),
+                                     EXIT_MESSAGE VARCHAR(2500),
+                                     LAST_UPDATED DATETIME,
+                                     JOB_CONFIGURATION_LOCATION VARCHAR(2500) NULL,
+                                     CONSTRAINT JOB_INST_EXEC_FK FOREIGN KEY (JOB_INSTANCE_ID)
+                                         REFERENCES BATCH_JOB_INSTANCE(JOB_INSTANCE_ID)
+);
+
+CREATE TABLE BATCH_JOB_EXECUTION_CONTEXT (
+                                             JOB_EXECUTION_ID BIGINT NOT NULL PRIMARY KEY,
+                                             SHORT_CONTEXT VARCHAR(2500) NOT NULL,
+                                             SERIALIZED_CONTEXT TEXT,
+                                             CONSTRAINT JOB_EXEC_CTX_FK FOREIGN KEY (JOB_EXECUTION_ID)
+                                                 REFERENCES BATCH_JOB_EXECUTION(JOB_EXECUTION_ID)
+);
+
+CREATE TABLE BATCH_JOB_SEQ (
+                               ID BIGINT NOT NULL,
+                               UNIQUE_KEY CHAR(1) NOT NULL,
+                               constraint UNIQUE_KEY_UN unique (UNIQUE_KEY)
+) ENGINE=InnoDB;
+
+CREATE TABLE BATCH_JOB_EXECUTION_SEQ (
+                                         ID BIGINT NOT NULL,
+                                         UNIQUE_KEY CHAR(1) NOT NULL,
+                                         constraint UNIQUE_KEY_UN unique (UNIQUE_KEY)
+) ENGINE=InnoDB;
+
+CREATE TABLE BATCH_STEP_EXECUTION  (
+                                       STEP_EXECUTION_ID BIGINT  NOT NULL PRIMARY KEY,
+                                       VERSION BIGINT NOT NULL,
+                                       STEP_NAME VARCHAR(100) NOT NULL,
+                                       JOB_EXECUTION_ID BIGINT NOT NULL,
+                                       CREATE_TIME DATETIME(6) NOT NULL,
+                                       START_TIME DATETIME(6) DEFAULT NULL,
+                                       END_TIME DATETIME(6) DEFAULT NULL,
+                                       STATUS VARCHAR(10),
+                                       COMMIT_COUNT BIGINT,
+                                       READ_COUNT BIGINT,
+                                       FILTER_COUNT BIGINT,
+                                       WRITE_COUNT BIGINT,
+                                       READ_SKIP_COUNT BIGINT,
+                                       WRITE_SKIP_COUNT BIGINT,
+                                       PROCESS_SKIP_COUNT BIGINT,
+                                       ROLLBACK_COUNT BIGINT,
+                                       EXIT_CODE VARCHAR(2500),
+                                       EXIT_MESSAGE VARCHAR(2500),
+                                       LAST_UPDATED DATETIME(6),
+                                       constraint JOB_EXEC_STEP_FK foreign key (JOB_EXECUTION_ID)
+                                           references BATCH_JOB_EXECUTION(JOB_EXECUTION_ID)
+) ENGINE=InnoDB;
+
+CREATE TABLE BATCH_STEP_EXECUTION_CONTEXT  (
+                                               STEP_EXECUTION_ID BIGINT NOT NULL PRIMARY KEY,
+                                               SHORT_CONTEXT VARCHAR(2500) NOT NULL,
+                                               SERIALIZED_CONTEXT TEXT,
+                                               constraint STEP_EXEC_CTX_FK foreign key (STEP_EXECUTION_ID)
+                                                   references BATCH_STEP_EXECUTION(STEP_EXECUTION_ID)
+) ENGINE=InnoDB;
+
+CREATE TABLE BATCH_STEP_EXECUTION_SEQ (
+                                          ID BIGINT NOT NULL,
+                                          UNIQUE_KEY CHAR(1) NOT NULL,
+                                          constraint UNIQUE_KEY_UN unique (UNIQUE_KEY)
+) ENGINE=InnoDB;
+
+
+INSERT INTO BATCH_STEP_EXECUTION_SEQ (ID, UNIQUE_KEY) select * from (select 0 as ID, '0' as UNIQUE_KEY) as tmp where not exists(select * from BATCH_STEP_EXECUTION_SEQ);
+INSERT INTO BATCH_JOB_EXECUTION_SEQ (ID, UNIQUE_KEY) select * from (select 0 as ID, '0' as UNIQUE_KEY) as tmp where not exists(select * from BATCH_JOB_EXECUTION_SEQ);
+INSERT INTO BATCH_JOB_SEQ (ID, UNIQUE_KEY) select * from (select 0 as ID, '0' as UNIQUE_KEY) as tmp where not exists(select * from BATCH_JOB_SEQ);
