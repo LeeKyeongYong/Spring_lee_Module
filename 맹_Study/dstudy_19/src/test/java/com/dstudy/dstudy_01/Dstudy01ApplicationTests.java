0bb2005c23454e9eee10eacdb4ac72fed77948a4
이경용 <sleekydz86@naver.com>
2025-01-27 21:27:15 +0900

    feat:jooq custom 실습

--- 맹_Study/dstudy_19/src/test/java/com/dstudy/dstudy_01/Dstudy01ApplicationTests.java
+++ 맹_Study/dstudy_19/src/test/java/com/dstudy/dstudy_01/Dstudy01ApplicationTests.java
@@ -1,10 +1,16 @@
 package com.dstudy.dstudy_01;
 
+import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Dstudy01ApplicationTests {
+
+    @Autowired
+    private DSLContext dslContext;
+
 
     @Test
     void contextLoads() {
