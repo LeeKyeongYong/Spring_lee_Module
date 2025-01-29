3f84b0724d371bd0365921b3fef847a79965d390
이경용 <sleekydz86@naver.com>
2025-01-29 23:25:35 +0900

    feat:Set up Builder Design Pattern environment

---
+++ BuilderDesignPattern/src/main/java/com/bp/example/BuilderDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.bp.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderDesignPatternApplication.class, args);
+    }
+
+}
