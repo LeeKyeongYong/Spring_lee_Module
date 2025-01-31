e7af33b709a4b6cfeff154bf4e6c17ee9016ce3b
이경용 <sleekydz86@naver.com>
2025-01-31 13:03:29 +0900

    feat:{name="Paul", age=20} 이라는 내용을 기반으로 JWT 생성

--- NextSpring/src/test/java/com/study/nextspring/domain/member/service/AuthTokenServiceTest.java
+++ NextSpring/src/test/java/com/study/nextspring/domain/member/service/AuthTokenServiceTest.java
@@ -1,11 +1,17 @@
 package com.study.nextspring.domain.member.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+import java.security.Key;
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,5 +26,33 @@ public class AuthTokenServiceTest {
     @DisplayName("authTokenService 서비스가 존재한다.")
     void t1() {
         assertThat(authTokenService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jjwt 로 JWT 생성, {name=\"Paul\", age=23}")
+    void t2() {
+        // 토큰 만료기간 : 1년
+        int expireSeconds = 60 * 60 * 24 * 365;
+        // 토큰 시크릿 키
+        Key secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
+
+        Claims claims = Jwts.claims()
+                .add("name", "Paul")
+                .add("age", 23)
+                .build();
+
+        Date issuedAt = new Date();
+        Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
+
+        String jwt = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt = " + jwt);
     }
 }
