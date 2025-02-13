1583806868846bdb8046520ba23220f4395df3fa
이경용 <sleekydz86@naver.com>
2025-02-13 23:27:12 +0900

    feat:Enhance communication security by defining data types and utilizing auto-generated schema from Spring Boot Open API
    - Defined communication data types to improve security and ensure type safety during data transmission.
    - Integrated schema types automatically generated from Spring Boot's Open API documentation to standardize communication structures.

--- NextSpring/frontend/src/app/post/list/page.tsx
+++ NextSpring/frontend/src/app/post/list/page.tsx
@@ -1,6 +1,29 @@
+import type { components } from "@/lib/backend/apiV1/schema";
+type PostDto = components["schemas"]["PostDto"];
+
+type PostDto = {
+    id: number;
+    createDate: string;
+    modifyDate: string;
+    authorId: number;
+    authorName: string;
+    title: string;
+    published: boolean;
+    listed: boolean;
+};
+
+type PostItemPageDto = {
+    currentPageNumber: number;
+    pageSize: number;
+    totalPages: number;
+    totalItems: number;
+    items: PostDto[];
+};
+
 export default async function Page() {
     const response = await fetch("http://localhost:9090/api/v1/posts");
-    const body = await response.json();
+    const body:PostItemPageDto  = await response.json();
+
 
     return (
         <div>
@@ -14,10 +37,10 @@ export default async function Page() {
             <hr />
 
             <ul>
-                {body.content.map((item: any) => (
+                {body.content.map((item: PostDto) => (
                     <li key={item.id} className="border-[2px] border-[red] my-3">
                         <div>id : {item.id}</div>
-                        <div>createDate : {item.createDate}</div>
+                        <div>createDate : {item.}</div>
                         <div>modifyDate : {item.modifyDate}</div>
                         <div>authorId : {item.authorId}</div>
                         <div>authorName : {item.authorName}</div>
