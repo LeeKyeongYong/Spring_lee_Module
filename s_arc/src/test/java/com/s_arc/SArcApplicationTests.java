66a269b8680b94e0a6382293d751f9f1ae7ccbf8
이경용 <sleekydz86@naver.com>
2025-02-17 13:03:56 +0900

    feat:아키텍처 학습을위한 init

---
+++ s_arc/src/test/java/com/s_arc/SArcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s_arc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SArcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
