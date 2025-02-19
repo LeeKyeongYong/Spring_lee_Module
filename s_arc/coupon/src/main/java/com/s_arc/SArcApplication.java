9a4c69405fb1fca8837172eb7a826aeb78282538
이경용 <sleekydz86@naver.com>
2025-02-20 00:18:39 +0900

    feat:아키텍처 프로젝트 정리

---
+++ s_arc/coupon/src/main/java/com/s_arc/SArcApplication.java
@@ -0,0 +1,13 @@
+package com.s_arc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SArcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SArcApplication.class, args);
+	}
+
+}
