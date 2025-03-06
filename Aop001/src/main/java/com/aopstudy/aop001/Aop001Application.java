8da21bf0960cf78e198b59610c8c9f268de56321
이경용 <sleekydz86@naver.com>
2025-03-07 00:09:06 +0900

    feat: aop 리마인드 복습하기위해 셋팅 1

---
+++ Aop001/src/main/java/com/aopstudy/aop001/Aop001Application.java
@@ -0,0 +1,13 @@
+package com.aopstudy.aop001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aop001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aop001Application.class, args);
+    }
+
+}
---
+++ Aop001/src/test/java/com/aopstudy/aop001/Aop001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aopstudy.aop001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
