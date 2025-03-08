3f34022466b30470e432cddda589948ba92caffe
이경용 <sleekydz86@naver.com>
2025-03-08 22:03:55 +0900

    feat: aop 복습 리마인드 - 동적프록시 생성하는법 학습하기위해 셋팅

---
+++ aop003/src/main/java/com/study/aop003/Aop003Application.java
@@ -0,0 +1,13 @@
+package com.study.aop003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aop003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aop003Application.class, args);
+    }
+
+}
---
+++ aop003/src/test/java/com/study/aop003/Aop003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aop003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop003ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
