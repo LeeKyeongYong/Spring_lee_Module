14ef5fccfd99cdbd12a62964ecad2326c9e255b5
이경용 <sleekydz86@naver.com>
2025-03-11 18:22:09 +0900

    feat: aop의  리마인드 학습 포인트컷과 어바이스 정의 init
    - 프록시팩토리빈을 사용하여 어드바이스 포인트컷을 별도정의 학습

---
+++ aop005/src/main/java/com/proxypstudy/aop005/Aop005Application.java
@@ -0,0 +1,13 @@
+package com.proxypstudy.aop005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aop005Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aop005Application.class, args);
+    }
+
+}
---
+++ aop005/src/test/java/com/proxypstudy/aop005/Aop005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxypstudy.aop005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop005ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
