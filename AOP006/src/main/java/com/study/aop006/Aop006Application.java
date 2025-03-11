206aea704a77d3de2834a16504db55285feabf23
이경용 <sleekydz86@naver.com>
2025-03-12 01:02:35 +0900

    feat: aop 프로젝트 리마인드 6번째
    -   AspectJ 표현식 사용 학습을 위한  init

---
+++ AOP006/src/main/java/com/study/aop006/Aop006Application.java
@@ -0,0 +1,13 @@
+package com.study.aop006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aop006Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aop006Application.class, args);
+    }
+
+}
---
+++ AOP006/src/test/java/com/study/aop006/Aop006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aop006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop006ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
