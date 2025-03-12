66479bf345d84d6f194f16f4e2936da44d177391
이경용 <sleekydz86@naver.com>
2025-03-12 23:15:56 +0900

    feat:AOP 리마인드
    - 자동프록시 생성하기위해  프로젝트 Init

---
+++ Aop007/src/main/java/com/example/aop007/Aop007Application.java
@@ -0,0 +1,13 @@
+package com.example.aop007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aop007Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aop007Application.class, args);
+    }
+
+}
---
+++ Aop007/src/test/java/com/example/aop007/Aop007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aop007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop007ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
