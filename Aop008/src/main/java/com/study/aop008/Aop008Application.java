5a94802568684e5958964dd35b2fb7c06a8931b4
이경용 <sleekydz86@naver.com>
2025-03-13 22:28:44 +0900

    feat: TrancationInterceptor빈의사용 프로젝트 Init

---
+++ Aop008/src/main/java/com/study/aop008/Aop008Application.java
@@ -0,0 +1,13 @@
+package com.study.aop008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aop008Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aop008Application.class, args);
+    }
+
+}
---
+++ Aop008/src/test/java/com/study/aop008/Aop008ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aop008;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop008ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
