d4bd4325e47226e564e529006871fce978657ec7
이경용 <sleekydz86@naver.com>
2025-03-15 17:03:17 +0900

    feat:HTTP(REST API) 및 gRPC 프로토콜을 동시에 지원하는 멀티 서버 설치

---
+++ grpc-multi-server/src/main/java/com/example/grpcmultiserver/GrpcMultiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpcmultiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcMultiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcMultiServerApplication.class, args);
+    }
+
+}
---
+++ grpc-multi-server/src/test/java/com/example/grpcmultiserver/GrpcMultiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcmultiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcMultiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
