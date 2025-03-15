29cb680a46581638c4b7bc4da609ebf8780af1e1
이경용 <sleekydz86@naver.com>
2025-03-15 21:34:06 +0900

    feat: aop 리마인드 하기위한 Init
    - @ Transactonal 사용 방법 리마은드 위해 Init

---
+++ Aop010/src/main/java/com/study/aop010/Aop010Application.java
@@ -0,0 +1,13 @@
+package com.study.aop010;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aop010Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aop010Application.class, args);
+    }
+
+}
---
+++ Aop010/src/test/java/com/study/aop010/Aop010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aop010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop010ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
