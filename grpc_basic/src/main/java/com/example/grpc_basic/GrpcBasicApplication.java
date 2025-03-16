a8ab9e5d8e32e5762f96211492f4dcbed9a4addd
이경용 <sleekydz86@naver.com>
2025-03-16 20:41:45 +0900

    feat: 중복방지하기위해 설정

--- grpc_basic/src/main/java/com/example/grpc_basic/GrpcBasicApplication.java
+++ grpc_basic/src/main/java/com/example/grpc_basic/GrpcBasicApplication.java
@@ -1,9 +1,10 @@
 package com.example.grpc_basic;
 
+import net.devh.boot.grpc.server.autoconfigure.GrpcHealthServiceAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {GrpcHealthServiceAutoConfiguration.class})
 public class GrpcBasicApplication {
 
     public static void main(String[] args) {
