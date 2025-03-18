aba9184e11fda5188915593ff35a3a17cd72767e
이경용 <sleekydz86@naver.com>
2025-03-18 19:54:37 +0900

    리팩토링 008 챕터 init

---
+++ ref008/src/main/java/com/tobystudy/ref008/Ref008Application.java
@@ -0,0 +1,13 @@
+package com.tobystudy.ref008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ref008Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ref008Application.class, args);
+    }
+
+}
---
+++ ref008/src/test/java/com/tobystudy/ref008/Ref008ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobystudy.ref008;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ref008ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
