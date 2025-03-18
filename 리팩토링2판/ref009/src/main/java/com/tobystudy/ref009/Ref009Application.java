ddebec2da55aabef2f0989b8107ebfde6cacc101
이경용 <sleekydz86@naver.com>
2025-03-18 19:56:46 +0900

    feat: 리팩토링 챕터 9 정리 학습 init
    - 챕터 8장 폴더로 이동
    - 챕터 9 폴더 생성

---
+++ 리팩토링2판/ref009/src/main/java/com/tobystudy/ref009/Ref009Application.java
@@ -0,0 +1,13 @@
+package com.tobystudy.ref009;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ref009Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ref009Application.class, args);
+    }
+
+}
---
+++ 리팩토링2판/ref009/src/test/java/com/tobystudy/ref009/Ref009ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobystudy.ref009;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ref009ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
